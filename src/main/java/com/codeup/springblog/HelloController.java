61c0ae53dd2cc112a2b34c24ef7300e8554cac04
victorpena13 <victor.pena1392@gmail.com>
2019-09-05 15:58:04 -0500

    first class exercises

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,44 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "<h2>Hello from Spring!</h2>";
+    }
+
+//    @PostMapping("/hello")
+//    @ResponseBody
+//    public String goodbye() {
+//        return
+//    }
+
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String goodbye(@PathVariable String name) {
+//        return "well hello" + " " + name + "!";
+//    }
+
+    @GetMapping("/goodbye")
+    @ResponseBody
+    public String goodbyeGoodbye() {
+        return "You waved goodbye";
+    }
+
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+
+
+
+
+}
