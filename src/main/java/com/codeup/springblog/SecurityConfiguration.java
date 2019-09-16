7da870fd7d8dea3206aae3b5b8957fffe09c4496
victorpena13 <victor.pena1392@gmail.com>
2019-09-16 17:09:50 -0500

    fixed upload. still need to work on json

--- src/main/java/com/codeup/springblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/SecurityConfiguration.java
@@ -24,6 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
         auth
                 .userDetailsService(usersLoader) // How to find users by their username
                 .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
@@ -33,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and().csrf().disable()
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
